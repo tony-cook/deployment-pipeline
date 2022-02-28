27b4c033539453925403084f97f4162d4f0246b5
Rowan Craik <Rowan5654@hotmail.com>
2022-03-01 11:31:19 +1300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <header class="top-bar spread">
+    <nav class="top-bar-nav">
+      <router-link to="/" class="top-bar-link">
+        <i class="icofont-spoon-and-fork"></i>
+        <span>Home</span>
+      </router-link>
+      <router-link to="/products" class="top-bar-link">
+        <span>Products</span>
+      </router-link>
+      <router-link to="/past-orders" class="top-bar-link">
+        <span>Past Orders</span>
+      </router-link>
+    </nav>
+    <div @click="toggleSidebar" class="top-bar-cart-link">
+      <i class="icofont-cart-alt icofont-1x"></i>
+      <span>Cart ({{ totalQuantity }})</span>
+    </div>
+  </header>
+  <router-view :inventory="inventory" :addToCart="addToCart" />
+
+  <SidebarView
+    v-if="showSidebar"
+    :toggle="toggleSidebar"
+    :cart="cart"
+    :inventory="inventory"
+    :remove="removeItem"
+  />
+</template>
+
+<script>
+import SidebarView from '@/components/SidebarView.vue'
+import food from '../public/food.json'
+
+export default {
+  components: {
+    SidebarView
+  },
+  data () {
+    return {
+      showSidebar: false,
+      inventory: food,
+      cart: {}
+    }
+  },
+  computed: {
+    totalQuantity () {
+      return Object.values(this.cart).reduce((sum, currentValue) => {
+        return sum + currentValue
+      }, 0)
+    }
+  },
+  methods: {
+    addToCart (name, quantity) {
+      if (!this.cart[name]) this.cart[name] = 0
+      this.cart[name] += quantity
+    },
+    toggleSidebar () {
+      this.showSidebar = !this.showSidebar
+    },
+    removeItem (name) {
+      delete this.cart[name]
+    }
+  },
+  async mounted () {
+    const res = await fetch('../public/food.json')
+    const data = await res.json()
+    this.inventory = data
+  }
+}
+</script>
